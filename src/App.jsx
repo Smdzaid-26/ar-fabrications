4b0da91334ca2b97e84beca871c9cd7c017415a0
Smdzaid <smdzaid92@gmail.com>
2026-03-15 14:33:07 +0530

    Customsteel fixed again

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import Home from "./pages/Home";
 import ServiceDetail from "./pages/ServiceDetail";
 import CustomSteel from "./components/services/CustomSteel";
 
-
 function App() {
   return (
     <Routes>
+
       <Route path="/" element={<Home />} />
-      <Route path="/services/:slug" element={<ServiceDetail />} />
+
+      {/* Custom Steel Page */}
       <Route path="/services/custom-steel" element={<CustomSteel />} />
+
+      {/* Dynamic Service Pages */}
+      <Route path="/services/:slug" element={<ServiceDetail />} />
+
     </Routes>
   );
 }
